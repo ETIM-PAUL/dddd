ad37bd24176e03aaf43e74ff07572878a0dfe14f
ETIM-PAUL <etimpaul22@gmail.com>
2022-08-15 03:23:00 +0100

    enabled login using spotify and redirecting to main page if successful

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import SpotifyLogin from "./pages/login";
+import SpotifyPlayer from "./pages/main";
+import { getTokenFromUrl } from "./utils/utilFunctions";
 
 function App() {
-  return <SpotifyLogin />;
+  const [spotifyLoginToken, setSpotifyLoginToken] = useState("");
+
+  useEffect(() => {
+    const hashed = getTokenFromUrl();
+    window.location.hash = "";
+    const loginToken = hashed.access_token;
+
+    if (loginToken) {
+      setSpotifyLoginToken(loginToken);
+    }
+  }, []);
+
+  return <div>{spotifyLoginToken ? <SpotifyPlayer /> : <SpotifyLogin />}</div>;
 }
 
 export default App;
---
+++ src/adapters/spotify-config.js
@@ -0,0 +1,15 @@
+const authEndpoint = "https://accounts.spotify.com/authorize";
+const redirectUri = "http://localhost:3000/";
+const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+];
+
+export const spotifyLoginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
