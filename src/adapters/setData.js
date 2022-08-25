33402bf05e37629cc059802b475d5fde675d3fb7
ETIM-PAUL <etimpaul22@gmail.com>
2022-08-26 00:45:12 +0100

    pause playing state of track

--- src/adapters/setData.js
+++ src/adapters/setData.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 export const changeVolume = async (token, value) => {
-  console.log(value);
   try {
     const response = await axios.put(
       "https://api.spotify.com/v1/me/player/volume",
@@ -10,6 +9,43 @@ export const changeVolume = async (token, value) => {
         params: {
           volume_percent: parseInt(value),
         },
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const startPlayer = async (token, value) => {
+  try {
+    await axios.put(
+      "https://api.spotify.com/v1/me/player/volume",
+      {},
+      {
+        params: {
+          volume_percent: parseInt(value),
+        },
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const pausePlaying = async (token) => {
+  try {
+    await axios.put(
+      "https://api.spotify.com/v1/me/player/pause",
+      {},
+      {
         headers: {
           Authorization: "Bearer " + token,
           "Content-Type": "application/json",
